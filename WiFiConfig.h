5200a9572be726ead571dfe666d68969738b5cac
Juerd Waalboer <juerd@tnx.nl>
2020-04-16 04:21:16 +0200

    Implement custom configuration parameters

--- WiFiConfig.h
+++ WiFiConfig.h
@@ -13,6 +13,10 @@ class WiFiConfigClass {
         bool connect(bool portal = true, int wait_seconds = 30);
         void portal();
         String hostname;
+        String string(String name, String init = "", String label = "");
+        String string(String name, unsigned int max_length, String init = "", String label = "");
+        long integer(String name, long init = 0, String label = "");
+        long integer(String name, long min, long max, long init = 0, String label = "");
 
         TCallback onConnect;
         TCallbackInt onWaitLoop;
@@ -24,6 +28,15 @@ class WiFiConfigClass {
         TCallback onPortalWaitLoop;
     private:
 };
+
+/*class WiFiConfigString: public String {
+    public:
+        WiFiConfigString(const char *p) : String(p) {}
+        WiFiConfigString(const String &s) : String(s) {}
+        String name;
+    private:
+};
+*/
 
 extern WiFiConfigClass WiFiConfig;
 
